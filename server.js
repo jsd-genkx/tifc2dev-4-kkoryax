ea5e5899b2e13f5cba64a1448db762838b8474dd
kkoryax <orachun.wsst@gmail.com>
2025-04-17 15:21:16 +0700

    setup(server): setup server.js with PORT and get method

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
-// 🎯 Import the Express module, which helps us build a web server
-// ⬇️ Code here
 const express = require();
-// Create an instance of an Express app
-// ⬇️ Code here
-const app = "?";
-// Define the port number where the server will listen for requests
-// ⬇️ Code here
-const port = "?";
-// Use Express's built-in middleware to parse incoming JSON data in request bodies
-// ⬇️ Code here
-app.use();
-// Create a temporary array to store posts in memory (this simulates a database)
-// ⬇️ Code here
-let posts = "?";
+const app = express();
 
-// Set up a route to handle POST requests made to "/posts"
-// This route will receive data from the client and save it as a new post
+const hostname = 'localhost'
+const PORT = '3000'
 
-app.post("/posts", (req, res) => {
-  // ⬇️ Code here
-  const {} = req.body;
-  // Example: Save the post to a data store
-  // Create a new post object with a unique id and the received title and content
-  const newPost = {};
-  // Add the new post to the posts array
-  posts.push();
-  // Respond with a 201 status code (Created) and send back the new post as JSON
-  res.status().json();
-});
+//create middleware for parse JSON
+app.use(express.json())
 
-// Start the server and have it listen on the specified port
-// Log a message to let us know the server is running
-app.listen(port, () => {
-  // ⬇️ Code here
+let posts = [];
+
+app.get("/", (req, res) => {
+  console.log("Express API is running...")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server start running on http://${hostname}:${PORT}`)
 });
