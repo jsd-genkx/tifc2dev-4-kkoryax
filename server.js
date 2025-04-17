4cc0f850ad245401e182959999d5e7fcfd19c494
kkoryax <orachun.wsst@gmail.com>
2025-04-17 15:38:23 +0700

    feat(server): added route.rest from REST extension to stimulate post request

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-const express = require();
+const express = require('express');
 const app = express();
 
 const hostname = 'localhost'
 const PORT = '3000'
 
 //create middleware for parse JSON
-app.use(express.json())
+app.use(express.json());
 
 let posts = [];
 
 app.get("/", (req, res) => {
-  console.log("Express API is running...")
-})
+  res.send("Express API is running...")
+});
+
+app.get("/post", (req, res) => {
+  res.json(posts)
+});
+
+app.post("/post", (req, res) => {
+  const {title , content} = req.body
+  const newPost = {
+    id: posts.length + 1,
+    title: title,
+    content: content
+  };
+  posts.push(newPost);
+
+  res.status(200).json(newPost);
+});
 
 app.listen(PORT, () => {
   console.log(`Server start running on http://${hostname}:${PORT}`)
